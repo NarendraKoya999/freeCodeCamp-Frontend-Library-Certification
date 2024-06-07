3327f33ae3a551db8e3d7cdbeb0f5c3ca9f19323
Koya Narendra <narendrakoya134679@gmail.com>
2024-06-07 13:01:40 +0530

    Fifth Commit

--- Build a 25+5 Clock/src/App.jsx
+++ Build a 25+5 Clock/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import BreakControl from "./components/BreakControl";
+import SessionControl from "./components/SessionControl";
+import Timer from "./components/Timer";
+import "./index.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <h1>Pomodoro Timer</h1>
+      <div className="controls">
+        <BreakControl />
+        <SessionControl />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Timer />
+    </div>
+  );
+};
 
-export default App
+export default App;
