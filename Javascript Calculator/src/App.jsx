6bc44ec7542aaed15fd5f3158b6d94f654165006
Koya Narendra <narendrakoya134679@gmail.com>
2024-06-07 10:59:06 +0530

    first commit

---
+++ Javascript Calculator/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Calculator from "./Components/Calculator";
+import "./App.css"; // Assuming you have some CSS styles in this file
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Calculator</h1>
+      </header>
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
